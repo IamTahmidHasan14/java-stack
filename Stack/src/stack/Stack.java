3a0ce93acf694e975afc865828405b97f49b82a5
Tahmid Hasan <67729687+IamTahmidHasan14@users.noreply.github.com>
2023-07-25 19:41:06 +0600

    Add files via upload

---
+++ Stack/src/stack/Stack.java
@@ -0,0 +1,42 @@
+package stack;
+public class Stack {
+
+    public static void push(Stack<Integer> stack) 
+    { 
+        for(int i = 0; i < 5; i++){ 
+            stack.push(i); 
+        } 
+    } 
+    public static void pop(Stack<Integer> stack) 
+    { 
+        System.out.println("Pop Operation:");   
+        for(int i = 0; i < 5; i++){ 
+            Integer y = (Integer) stack.pop(); 
+            System.out.println(y); 
+        } 
+    } 
+    public static void peek(Stack<Integer> stack) 
+    { 
+        Integer element = (Integer) stack.peek(); 
+        System.out.println("Element on stack top: " + element); 
+    }
+    public static void search(Stack<Integer> stack, int element) 
+    { 
+        Integer pos = (Integer) stack.search(element); 
+  
+        if(pos == -1) 
+            System.out.println("Element not found"); 
+        else
+            System.out.println("Element is found at position: " + pos); 
+    }   
+    public static void main (String[] args) 
+    { 
+        Stack<Integer> stack = new Stack<Integer>(); 
+        push(stack); 
+        pop(stack); 
+        push(stack); 
+        peek(stack); 
+        search(stack, 2); 
+        search(stack, 6); 
+    }   
+}
